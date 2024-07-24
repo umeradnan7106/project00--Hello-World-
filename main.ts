77132175108be269e4cca0c7fac23cdd25f38c35
umeradnan7106 <120960218+umeradnan7106@users.noreply.github.com>
2024-07-24 05:38:31 -0700

    first commit

---
+++ main.ts
@@ -0,0 +1,73 @@
+console.log("Hello World");
+
+let  message = "Hello World";
+console.log(message);
+
+
+const it = "Classes";
+// it = classes;
+console.log(it);
+
+
+
+let firstName = "Umer";
+console.log(firstName);
+
+// this number is string
+let a = "22";
+console.log(a);
+
+// this is a number
+let b = 22;
+console.log(b);
+
+
+let fullName = "hamzah syed";
+// console.log("hello");
+// console.log("hello");
+// console.log("hello");
+// console.log("hello");
+
+
+let secondName = "Umer";
+let age = 23;
+// let isMarried = False ;
+let lastName: string = "22"
+
+console.log(secondName,age,lastName,);
+
+
+
+let num1 = "1";
+let num2 = "1";
+    //    "1" + "1" = "11"
+let result = num1 + num2;
+
+
+console.log(num1);
+console.log(num2);
+console.log(result);
+
+
+
+let num3 = 1;
+let num4 = 1;
+//    1 + 1 = 2
+let result2 = num3 + num4;
+console.log(result2);
+
+
+let num7 = 1;
+let num8 = "1";
+    //    1 + "1" = 11
+let result1 = num7 + num8;
+console.log(result1);
+
+let num5 = 2;
+let num6 = 100;
+console.log(num5 + num6);
+console.log(num5 - num6);
+console.log(num5 * num6);
+console.log(num5 / num6);
+console.log(num5 ** num6);
+console.log(num6 % num5);
